a4f73fe11add3f4545b99ccae401f4ca00c033fa
Gilvan <87739497+GilvanPedro@users.noreply.github.com>
2025-02-24 20:08:02 -0300

    Create media.java

---
+++ media.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class Main{
+  
+  public static void main(String[] args) throws IOException{
+    
+      var n1 = 40;
+      var n2 = 80;
+      var n3 = 70;
+
+    System.out.println("A Média é: "+ ((n1+n2+n3)/3));
+                       
+  }
+}
