ad720a3f7c4fd4b949dec2f3537839500e2f0c01
Gilvan <87739497+GilvanPedro@users.noreply.github.com>
2025-02-24 19:51:37 -0300

    Conta.java

---
+++ Conta.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        var A = 10;
+        var B = 9;
+        var C = -10;
+        var D = 4;
+        var E = 15;
+        var F = -7;
+
+        System.out.println("X = "+ (A+B));
+        System.out.println("Y = "+ (C+D));
+        System.out.println("Z = "+ (E+F));
+       
+ 
+    }
+ 
+}
