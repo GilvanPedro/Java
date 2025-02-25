eb3132c69c2feacdd0d3005787e747b2786c16a5
Gilvan <87739497+GilvanPedro@users.noreply.github.com>
2025-02-24 22:27:31 -0300

    Update circuloArea.java

--- circuloArea.java
+++ circuloArea.java
@@ -1,17 +1,14 @@
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
- 
-    public static void main(String[] args) throws IOException {
- 
-        var n = 3.14159;
-        var v1 = 2.00;
-        var v2 = 100.64;
-        var v3 = 150.00;
+    public static void main(String[] args){
+        
+        double v1, R;
+        Scanner ent = new Scanner(System.in);
+        
+        v1 = ent.nextDouble();  
+        R = (3.14159*v1*v1);
 
-        System.out.println("A = "+ (n*v1*v1));
-        System.out.println("B = "+ (n*v2*v2));
-        System.out.println("C = "+ (n*v3*v3));
+        System.out.printf("A=%.4f\n", R);
     }
- 
 }
